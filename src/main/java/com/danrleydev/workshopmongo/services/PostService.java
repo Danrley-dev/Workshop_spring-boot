f10fab3c6d4b57b8990acdb494340f7bf50469bf
Danrleydev <danrleysouza53@gmail.com>
2022-09-05 20:14:20 -0300

    obtendo post com ID

---
+++ src/main/java/com/danrleydev/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.danrleydev.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danrleydev.workshopmongo.domain.Post;
+import com.danrleydev.workshopmongo.repository.PostRepository;
+import com.danrleydev.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+}
