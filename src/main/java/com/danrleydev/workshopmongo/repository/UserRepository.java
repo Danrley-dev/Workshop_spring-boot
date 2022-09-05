aa3f5b66b3d14dd5248189a485595c7fe897798d
Danrleydev <danrleysouza53@gmail.com>
2022-09-05 14:09:53 -0300

    conectando ao mongoDB com repository e service

---
+++ src/main/java/com/danrleydev/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.danrleydev.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danrleydev.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
